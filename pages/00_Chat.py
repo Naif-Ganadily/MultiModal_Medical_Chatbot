e62d45a69b0227012fc8b0152bcec5a76beca5c5
Naif Ganadily <ganadilynaif@gmail.com>
2024-04-25 11:40:24 -0700

    Added

--- pages/00_Chat.py
+++ pages/00_Chat.py
@@ -1,1 +1,26 @@
 # Home Page
+import streamlit as st
+from utilities.icon import page_icon
+
+
+st.set_page_config(
+    page_title="Medical Chat",
+    page_icon="💬",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+
+
+
+
+def main():
+    """
+    The main function that runs the application.
+    """
+
+    page_icon("💬")
+    st.subheader("Medical Chat Playground", divider="red", anchor=False)
+
+if __name__ == "__main__":
+    main()
